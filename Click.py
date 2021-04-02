d2ebdd6536977acef4c5cd6bd24ad1ddb94db843
Melroy Caeiro <71640510+MelroyCaeiro@users.noreply.github.com>
2021-04-02 10:29:10 +0800

    Create Click.py

---
+++ Click.py
@@ -0,0 +1,31 @@
+import serial
+import pyautogui, time
+import random as rd
+import sys
+pyautogui.FAILSAFE= True
+
+def Click():
+    pyautogui.click(interval=0.02)
+
+try:
+    arduino = serial.Serial("COM3", timeout=1, baudrate=9600)
+except:
+    print('Please check the port')
+
+time.sleep(6)
+
+while True:
+    lr = arduino.readline()
+    nl = lr.decode("utf-8")
+    ll = int(nl)
+
+    cond = (ll<20 or ll>2000)
+
+    if cond==True:
+        Click()
+        print("go")
+    else:
+        print("stop")
+        pass
+
+    #time.sleep(0.2) # for CS
